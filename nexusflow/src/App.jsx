4daa9618dbd66631b85fc54284e671c2c3bffc89
Patrick Mutua <pawatechsys@gmail.com>
2026-01-28 12:04:39 +0300

    initial commit

--- nexusflow/src/App.jsx
+++ nexusflow/src/App.jsx
@@ -1,5 +1,20 @@
 import { useState } from "react";
+import Navbar from "./components/NavBar/Navbar";
+import Herosection from "./components/HeroSection/Herosection";
+import Services from "./components/OurServices/Services";
+import About from "./components/AboutUs/About";
+import Footer from "./components/Footer/Footer.jsx";
 
-function App() {}
+function App() {
+  return (
+    <div className="bg-gray-50 w-full">
+      <Navbar />
+      <Herosection />
+      <Services />
+      <About />
+      <Footer />
+    </div>
+  );
+}
 
 export default App;
